e7e74c5aefb611b0b858bdc5aef49c492f26b68c
Fabian Gonzalez <fagm18@gmail.com>
2022-04-28 13:18:49 -0600

    Added app navigation

--- app/src/main/java/cr/una/eif409/frontend/servicesapp/MainActivity.kt
+++ app/src/main/java/cr/una/eif409/frontend/servicesapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package cr.una.eif409.frontend.servicesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Setup the navigation controller
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Setup the top app bar
+        val appBarConfiguration =
+            AppBarConfiguration(
+                setOf(
+                    R.id.homeFragment,
+                    R.id.newServiceFragment,
+                    R.id.myServicesFragment,
+                    R.id.profileFragment,
+                )
+            )
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
+        // Setup the bottom navigation
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/cr/una/eif409/frontend/servicesapp/fragments/MyServicesFragment.kt
@@ -0,0 +1,18 @@
+package cr.una.eif409.frontend.servicesapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cr.una.eif409.frontend.servicesapp.R
+
+class MyServicesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_services, container, false)
+    }
+}
