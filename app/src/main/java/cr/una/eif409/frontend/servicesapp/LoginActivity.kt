29cf12d16ef90d532f275b5b5cba7d5534f0d018
Fabian Gonzalez <fagm18@gmail.com>
2022-04-29 22:05:01 -0600

    Implementation of the login screen

---
+++ app/src/main/java/cr/una/eif409/frontend/servicesapp/LoginActivity.kt
@@ -0,0 +1,25 @@
+package cr.una.eif409.frontend.servicesapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import cr.una.eif409.frontend.servicesapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionListeners()
+    }
+
+    private fun setupActionListeners() {
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
