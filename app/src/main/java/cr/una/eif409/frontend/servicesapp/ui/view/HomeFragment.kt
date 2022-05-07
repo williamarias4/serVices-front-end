53ab1a490d1de853b697738d406afe686a733160
Fabian Gonzalez <fagm18@gmail.com>
2022-05-06 21:41:30 -0600

    Added button to logout to the toolbar

--- app/src/main/java/cr/una/eif409/frontend/servicesapp/ui/view/HomeFragment.kt
+++ app/src/main/java/cr/una/eif409/frontend/servicesapp/ui/view/HomeFragment.kt
@@ -1,14 +1,11 @@
 package cr.una.eif409.frontend.servicesapp.ui.view
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import cr.una.eif409.frontend.servicesapp.R
-import cr.una.eif409.frontend.servicesapp.core.SharedApp
 
 class HomeFragment : Fragment() {
 
@@ -16,15 +13,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        view.findViewById<Button>(R.id.button_logout).setOnClickListener {
-            SharedApp.preferences.username = ""
-            val intent = Intent(context, AuthActivity::class.java)
-            startActivity(intent)
-            activity?.finish()
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
--- app/src/main/java/cr/una/eif409/frontend/servicesapp/ui/view/MainActivity.kt
+++ app/src/main/java/cr/una/eif409/frontend/servicesapp/ui/view/MainActivity.kt
@@ -1,6 +1,9 @@
 package cr.una.eif409.frontend.servicesapp.ui.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
@@ -8,6 +11,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import cr.una.eif409.frontend.servicesapp.R
+import cr.una.eif409.frontend.servicesapp.core.SharedApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,11 +33,37 @@ class MainActivity : AppCompatActivity() {
                     R.id.profileFragment,
                 )
             )
+        
         findViewById<Toolbar>(R.id.toolbar)
             .setupWithNavController(navController, appBarConfiguration)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         // Setup the bottom navigation
         findViewById<BottomNavigationView>(R.id.bottom_nav)
             .setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_logout -> {
+            logout()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout() {
+        SharedApp.preferences.username = ""
+        val intent = Intent(this, AuthActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
